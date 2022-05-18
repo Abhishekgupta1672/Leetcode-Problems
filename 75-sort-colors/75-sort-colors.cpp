d2fb1fc1de637798a0dbb260ec7316845627da0d
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-05-18 23:33:18 +0530

    Time: 2 ms (60.27%), Space: 8.4 MB (25.26%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int i, key, j;
+        int n = arr.size();
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    }
+};
