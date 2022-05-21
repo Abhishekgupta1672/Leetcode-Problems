f2d17c8c214f56d0a7e8fb1d2571f4743b0846e5
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-05-21 20:37:11 +0530

    Time: 140 ms (36.71%), Space: 14.5 MB (34.02%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,7 +4,6 @@ public:
     int rec(int amount,int n,vector<int> &coins,vector<vector<int>> &dp){
         if(amount == 0 )
             return dp[amount][n] =  0;
-        
         if(n == 0) return 100005;
         if(dp[amount][n] != -1) return dp[amount][n];
         if(amount >= coins[n-1]){
@@ -13,8 +12,16 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n =coins.size();
-        vector<vector<int>> dp(amount+1,vector<int>(n+1,-1));
-        rec(amount,n,coins,dp);
-        return dp[amount][n] >= 100005?-1:dp[amount][n];
+        vector<int> dp(amount+1,0);
+        
+        for(int i = 1 ; i <= amount ; i++){
+            dp[i] = 100005;
+            for(int j = 1 ; j <= n ; j++){
+                if(i >= coins[j-1]){
+                    dp[i] = min(dp[i-coins[j-1]]+1,dp[i]);
+                }
+            }
+        }
+        return dp[amount] >= 100005?-1:dp[amount];
     }
 };
