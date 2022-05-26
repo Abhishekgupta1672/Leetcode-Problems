c63c8c9977eced5c4423d84504870ce7e05aab65
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-05-26 17:07:43 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (89.33%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        vector<int>res;
+        int curr = n%2;
+        n/=2;
         while(n)
         {
-            res.push_back(n%2);
-            n/=2;
-        }
-        for(int i=1;i<res.size();i++)
-        {
-            if(res[i]==res[i-1])
+            if(curr ==n%2)
                 return false;
+            curr = n%2;
+            n/=2;
         }
         return true;
     }
