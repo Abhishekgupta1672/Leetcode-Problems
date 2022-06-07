bb5095a696625114b171e751b9d3d2706713c19f
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-06-07 14:23:10 +0530

    Time: 5 ms (18.07%), Space: 7 MB (56.12%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -0,0 +1,44 @@
+class MyQueue {
+public:
+    stack<int>s1,s2;
+    int front;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(s1.empty())
+            front = x;
+        s1.push(x);
+    }
+    
+    int pop() {
+        if(s2.empty())
+        {
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int p = s2.top();
+        s2.pop();
+        return p;
+    }
+    
+    int peek() {
+        if(s2.empty())
+        {
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
