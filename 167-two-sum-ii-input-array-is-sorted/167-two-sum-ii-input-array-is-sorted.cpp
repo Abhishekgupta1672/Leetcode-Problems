6b9b113164b24b38b2ad26ab9466ac1774aa1984
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-06-17 18:49:35 +0530

    Time: 31 ms (11.42%), Space: 15.7 MB (42.75%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -4,12 +4,10 @@ public:
         int low = 0 , high = nums.size()-1;
         while(low<high)
         {
-            if(nums[low]+ nums[high]==target)
+            if(nums[low]+nums[high]==target)
                 return {low+1,high+1};
-            else if(nums[low]+ nums[high]>target)
-            {
+            else if(nums[low]+nums[high] > target)
                 high--;
-            }
             else
                 low++;
         }
