234e6d813754d7b283618fc44b52c0eca9aedc35
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-06-25 14:57:14 +0530

    Time: 49 ms (27.45%), Space: 27.1 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -5,23 +5,17 @@ public:
         {
             if(nums[i]>nums[i+1])
             {
-                 if(i-1>=0&&nums[i-1]>nums[i+1])
-                {
-                    nums[i+1]=nums[i];
-                }
+                if(i-1>=0 && nums[i-1]>nums[i+1])
+                    nums[i+1] = nums[i];
                 else
-                {
-                    nums[i]=nums[i+1];
-                }
+                    nums[i] = nums[i+1];
                 break;
             }
         }
         for(int i=0;i<nums.size()-1;i++)
         {
             if(nums[i]>nums[i+1])
-            {
                 return false;
-            }
         }
         return true;
     }
