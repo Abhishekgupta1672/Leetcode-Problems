ad39dc3ac2cb5c0377b67392f8eaf7d1c405fc52
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-06-29 19:46:55 +0530

    Time: 0 ms (100.00%), Space: 10 MB (44.08%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -1,10 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==q)
-            return true;
-        if((p==NULL && q!=NULL) || (p!=NULL && q==NULL) || p->val!=q->val)
-            return false;
-        return isSameTree(p->left , q->left) && isSameTree(p->right , q->right);
+        if(!p || !q)
+            return p==q;
+        queue<TreeNode*>q1 , q2;
+        q1.push(p);
+        q2.push(q);
+        while(!q1.empty() && !q2.empty())
+        {
+            TreeNode* t1 = q1.front();
+            q1.pop();
+            TreeNode* t2 = q2.front();
+            q2.pop();
+            if(t1->val != t2->val) 
+                return false;
+            if((t1->left ==NULL && t2->left!=NULL) || (t1->left !=NULL && t2->left==NULL))
+                return false;
+            if((t1->right ==NULL && t2->right!=NULL) || (t1->right !=NULL && t2->right==NULL))
+                return false;
+            if(t1->left && t2->left)
+            {
+                q1.push(t1->left);
+                q2.push(t2->left);
+            }
+            if(t1->right && t2->right)
+            {
+                 q1.push(t1->right);
+                 q2.push(t2->right);
+            }
+           }
+        return true;
     }
 };
