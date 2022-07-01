dea01f0bebcf6d469e99e9c004e0ecbf2641f8ef
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-01 16:07:36 +0530

    Time: 4 ms (75.09%), Space: 8.7 MB (89.17%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int bsearch(vector<int>&nums , int l , int r)
-    {
-        if(l==r)
-            return l;
-        int mid = l+(r-l)/2;
-        if(nums[mid]>nums[mid+1])
-            return bsearch(nums , l , mid);
-        else
-            return bsearch(nums , mid+1 , r);
-    }
     int findPeakElement(vector<int>& nums) {
-        return bsearch(nums , 0 , nums.size()-1);
+        int l = 0 , r = nums.size()-1;
+        while(l<r)
+        {
+            int mid = l+(r-l)/2;
+            if(nums[mid]>nums[mid+1])
+                r = mid;
+            else
+                l = mid+1;
+        }
+        return l;
     }
 };
