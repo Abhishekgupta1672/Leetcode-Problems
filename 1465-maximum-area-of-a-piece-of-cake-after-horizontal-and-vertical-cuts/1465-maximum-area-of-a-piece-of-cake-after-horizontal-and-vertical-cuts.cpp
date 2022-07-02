852d3a756a61075966605238286a466d0cfd43a4
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-02 11:50:28 +0530

    Time: 129 ms (22.64%), Space: 32.3 MB (76.96%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    const long long int mod = 1000000007;
-
-    int maxArea(int h, int w, vector<int>& hc, vector<int>& vc) {
-        sort(hc.begin() , hc.end());
-        sort(vc.begin() , vc.end());
-        
-        vector<int>hh = {hc[0]};
-        for(int i=1;i<hc.size();i++)
+    int maxHeight(int h , vector<int>&horizontalCuts)
+    {
+        sort(horizontalCuts.begin() , horizontalCuts.end());
+        int sz = horizontalCuts.size();
+        int maxH = max(horizontalCuts[0] ,h-horizontalCuts[sz-1]);
+        for(int i=1;i<sz;i++)
         {
-            hh.push_back(hc[i]-hc[i-1]);
+            maxH = max(maxH , horizontalCuts[i]-horizontalCuts[i-1]);
         }
-        hh.push_back(h-hc[hc.size()-1]);
-        
-        vector<int>vh = {vc[0]};
-        for(int i=1;i<vc.size();i++)
-        {
-            vh.push_back(vc[i]-vc[i-1]);
-        }
-        vh.push_back(w-vc[vc.size()-1]);
-        
-        long long int maxH = *max_element(hh.begin() , hh.end());
-        long long int maxV = *max_element(vh.begin() , vh.end());
-        return (int)(maxH%mod*maxV%mod);
-
+        return maxH;
+    }
+    
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) 
+    {
+        return (int)((long)maxHeight(h , horizontalCuts)*maxHeight(w , verticalCuts)%1000000007);
     }
 };
