bc6f8222f74bbcdb46e6d4caae6c2d3bd764e389
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-04 20:06:33 +0530

    Time: 31 ms (38.36%), Space: 8.7 MB (39.29%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -3,27 +3,28 @@ public:
     vector<int> findAnagrams(string s, string p) {
         vector<int>v1(26,0);
         vector<int>v2(26,0);
-        vector<int>result;
-        int m = p.size() , n = s.size();
-        if(m>n)
-            return result;
-        int right = 0 , left = 0;
-        for(int i=0;i<m;i++)
+        vector<int>res;
+        int m = s.size() , n = p.size();
+        if(n>m)
+            return res;
+        int l = 0 , r = 0;
+        for(int i=0;i<n;i++)
         {
             v1[p[i]-'a']++;
             v2[s[i]-'a']++;
-            right++;
+            r++;
         }
-        right-=1;
-        while(right<n){
+        r-=1;
+        while(r<m)
+        {
             if(v1==v2)
-                result.push_back(left);
-            right+=1;
-            if(right!=n)
-                v2[s[right]-'a']+=1;
-            v2[s[left]-'a']-=1;
-            left++;
+                res.push_back(l);
+            r+=1;
+            if(r!=m)
+                v2[s[r]-'a']+=1;
+            v2[s[l]-'a']-=1;
+            l++;
         }
-        return result;
+        return res;
     }
 };
