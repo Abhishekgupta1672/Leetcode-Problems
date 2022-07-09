1b8db3662cbe45eaa3c9a14b667c3dbc4de98156
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-09 15:43:18 +0530

    Time: 34 ms (93.17%), Space: 19.8 MB (61.12%) - LeetHub

---
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,19 @@
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>>pq;
+    int size;
+    KthLargest(int k, vector<int>& nums) {
+        size = k;
+        for(auto num:nums)
+        {
+            pq.push(num);
+            if(pq.size()>k) pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>size) pq.pop();
+        return pq.top();
+    }
+};
