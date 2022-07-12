8a335925e519826fd6f3ae951e422ed6e600a191
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-12 15:31:35 +0530

    Time: 915 ms (17.97%), Space: 10.2 MB (47.49%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool dfs(vector<int>&matchsticks , vector<int>&ans , int index , int target)
+    {
+        if(index==-1) return true;
+        for(int i=0;i<4;i++)
+        {
+            if(ans[i]+matchsticks[index]>target)
+                continue;
+            ans[i]+=matchsticks[index];
+            if(dfs(matchsticks,ans,index-1,target))
+                return true;
+            ans[i]-=matchsticks[index];
+        }
+        return false;
+    }
+    
+    bool makesquare(vector<int>& matchsticks) {
+        int sum = 0;
+        for(auto x: matchsticks)
+            sum+=x;
+        if(sum%4 != 0 || matchsticks.size()<4)
+            return false;
+        sort(matchsticks.begin(),matchsticks.end());
+        vector<int>ans(4,0);
+        return dfs(matchsticks,ans,matchsticks.size()-1,sum/4);
+    }
+};
