abe96c2857515249e2cb22b92ea9f7fba22992f0
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-12 12:47:26 +0530

    Time: 92 ms (18.46%), Space: 18.6 MB (24.84%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head || !head->next || !head->next->next) return ;
+        ListNode* temp = head;
+        stack<ListNode*>st;
+        int sz = 0;
+        while(temp!=NULL)
+        {
+            st.push(temp);
+            temp = temp->next;
+            sz++;
+        }
+        ListNode* newTemp = head;
+        for(int i=0;i<sz/2;i++)
+        {
+            ListNode* last = st.top();
+            st.pop();
+            last->next = newTemp->next;
+            newTemp->next = last;
+            newTemp = newTemp->next->next;
+        }
+        newTemp->next = NULL;
+    }
+};
