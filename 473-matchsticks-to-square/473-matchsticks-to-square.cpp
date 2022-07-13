1a6a4afa4967d69e0b46c0ad0ba3da5d1bf6d688
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-13 12:17:53 +0530

    Time: 892 ms (19.43%), Space: 10.1 MB (48.02%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    bool dfs(vector<int>&matchsticks,vector<int>&ans,int index,int target)
+    bool dfs(vector<int>&matchsticks , vector<int>&res , int index , int target)
     {
         if(index==-1) return true;
         for(int i=0;i<4;i++)
         {
-            if((ans[i]+matchsticks[index]>target) || (i>0 && ans[i]==ans[i-1]))
-                continue;
-            ans[i]+=matchsticks[index];
-            if(dfs(matchsticks,ans,index-1,target))
-                return true;
-            ans[i]-=matchsticks[index];
+            if(res[i]+matchsticks[index]>target) continue;
+            res[i]+=matchsticks[index];
+            if(dfs(matchsticks,res,index-1,target)) return true;
+            res[i]-=matchsticks[index];
         }
         return false;
     }
     bool makesquare(vector<int>& matchsticks) {
         int sum=0;
-        for(auto x:matchsticks)
-            sum+=x;
-        if(sum%4 != 0 || matchsticks.size()<4)
-            return false;
+        for(int X:matchsticks)
+            sum+=X;
+        if(sum%4!=0 || matchsticks.size()<4) return false;
         sort(matchsticks.begin(),matchsticks.end());
-        vector<int>ans(4,0);
-        return dfs(matchsticks,ans,matchsticks.size()-1,sum/4);
+        vector<int>res(4,0);
+        return dfs(matchsticks,res,matchsticks.size()-1,sum/4);
     }
 };
