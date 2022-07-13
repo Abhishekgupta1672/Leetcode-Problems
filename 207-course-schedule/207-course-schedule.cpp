122e1508248fd0b4e4e6d221964747821726d44e
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-13 18:08:52 +0530

    Time: 43 ms (30.20%), Space: 14.3 MB (38.10%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkForCycle(int s , vector<int>adj[] , vector<int>&vis)
+    {
+        if(vis[s]==1) return true;
+         if(vis[s]==0)
+         {
+            vis[s]=1;
+            for(auto edge : adj[s]){
+                if(checkForCycle(edge,adj,vis))
+                    return true;
+            }
+        }
+        vis[s]=2;
+        return false;
+    }
+    bool canFinish(int V, vector<vector<int>>& pre) {
+        vector<int>adj[V];
+        for(auto edge : pre)
+            adj[edge[1]].push_back(edge[0]);
+        vector<int>vis(V+1,0);
+        for(int i=0;i<V;i++)
+        {
+            if(checkForCycle(i,adj,vis))
+                return false;
+        }
+        return true;
+    }
+};
