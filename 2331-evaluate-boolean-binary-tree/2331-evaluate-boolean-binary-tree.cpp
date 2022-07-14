777ddc5a4dbc34fa2aa359b5481d37773c703004
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-14 23:28:37 +0530

    Time: 20 ms (65.09%), Space: 15 MB (35.53%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    bool solve(TreeNode* root)
-    {
-        if(!root)
-            return 0;
-        int left=solve(root->left);
-        int right=solve(root->right);
-        
-        if(root->left==NULL && root->right==NULL)
-            return root->val;
-        if(root->val==2)
-            return left || right;
-        if(root->val==3)
-            return left && right;
-        
-        return 0;
-    }
     bool evaluateTree(TreeNode* root) {
-        return solve(root);
+        if(!root->left && !root->right)
+            return root->val;
+        int left = evaluateTree(root->left);
+        int right = evaluateTree(root->right);
+        if(root->val==2)
+            return left||right;
+        else if(root->val==3)
+            return left&&right;
+        return 0;
     }
 };
