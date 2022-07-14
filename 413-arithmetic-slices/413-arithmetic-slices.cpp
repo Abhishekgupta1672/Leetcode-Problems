78852dcbd2f19c6b80cb417b1ebba8621a5c49c1
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-14 23:46:18 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (50.27%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -3,13 +3,11 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
         if(n<3) return 0;
-        int cur=0 , total=0;
+        int cur = 0 , total = 0;
         for(int i=2;i<n;i++)
         {
-            if(nums[i]-nums[i-1] == nums[i-1]-nums[i-2])
-                cur++;
-            else
-                cur=0;
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]) cur++;
+            else cur=0;
             total+=cur;
         }
         return total;
