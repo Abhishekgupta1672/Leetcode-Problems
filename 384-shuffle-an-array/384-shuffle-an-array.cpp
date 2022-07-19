f5fa2622048fef134dfe068511302b6b56ae3d21
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-19 11:19:12 +0530

    Time: 146 ms (10.63%), Space: 56.8 MB (99.43%) - LeetHub

---
+++ 384-shuffle-an-array/384-shuffle-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<int>original;
+    int n;
+public:
+    
+    Solution(vector<int>& nums) {
+        original = nums;
+        n = original.size();
+    }
+    
+    vector<int> reset() {
+        return original;
+    }
+    
+    vector<int> shuffle() {
+        vector<int>shuffled = original;
+        int leftsize=n;
+        for(int i=n-1;i>=0;i--)
+        {
+            int j = rand()%leftsize;
+            swap(shuffled[i],shuffled[j]);
+            leftsize--;
+        }
+        return shuffled;
+    }
+};
