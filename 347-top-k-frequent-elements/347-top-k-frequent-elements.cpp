2b09025c3f05a0075fa3055cb9dcdbad00f73ef9
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-19 10:31:22 +0530

    Time: 36 ms (15.91%), Space: 13.6 MB (89.16%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    static bool cmp(pair<int , int>&a , pair<int , int>&b)
-    {
-        return a.second<b.second;
-    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int ,int>mp;
+        unordered_map<int,int>mp;
         for(auto x:nums)
             mp[x]++;
-        vector<pair<int ,int>>v(mp.begin() , mp.end());
-        sort(v.begin() , v.end() ,cmp);
-        vector<int>res;
-        for(auto it = v.rbegin() ; it!=v.rend();it++)
+        priority_queue<pair<int,int>>pq;
+        for(auto x:mp)
+            pq.push({x.second,x.first});
+        vector<int>v;
+        while(k--)
         {
-            if(k)
-            {
-                res.push_back(it->first);
-                k--;
-            }
+            v.push_back(pq.top().second);
+            pq.pop();
         }
-        return res;
+        return v;
     }
 };
