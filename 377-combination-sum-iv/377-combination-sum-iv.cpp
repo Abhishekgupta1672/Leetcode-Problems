03cc900142114c5ec9618723d66f6bd723324d5e
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-21 15:00:40 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (66.70%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
         vector<uint>dp(target+1,0);
         dp[0] = 1;
         for(int i=1;i<=target;i++)
@@ -8,6 +9,7 @@ public:
             for(auto num : nums)
             {
                 if(num<=i) dp[i]+=dp[i-num];
+                else break;
             }
         }
         return dp[target];
