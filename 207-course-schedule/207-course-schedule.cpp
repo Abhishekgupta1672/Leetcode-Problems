a5204e2968b88e8fedfa70edc27fa25e0619adf3
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-22 18:05:54 +0530

    Time: 39 ms (38.76%), Space: 13.9 MB (59.93%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    bool checkForCycle(int s , vector<int>adj[] , vector<int>&vis)
-    {
-        if(vis[s]==1) return true;
-         if(vis[s]==0)
-         {
-            vis[s]=1;
-            for(auto edge : adj[s]){
-                if(checkForCycle(edge,adj,vis))
-                    return true;
+    bool canFinish(int num, vector<vector<int>>& pre) {
+        vector<int>vis(num,0);
+        vector<int>adj[num];
+        for(auto itr:pre)
+        {
+            adj[itr[0]].push_back(itr[1]);
+            vis[itr[1]]++;
+        }
+        queue<int>q;
+        for(int i=0;i<num;i++)
+        {
+            if(vis[i]==0)
+                q.push(i);
+        }
+        vector<int>res;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            res.push_back(node);
+            for(auto itr:adj[node])
+            {
+                if(--vis[itr]==0)
+                    q.push(itr);
             }
         }
-        vis[s]=2;
-        return false;
-    }
-    bool canFinish(int V, vector<vector<int>>& pre) {
-        vector<int>adj[V];
-        for(auto edge : pre)
-            adj[edge[1]].push_back(edge[0]);
-        vector<int>vis(V,0);
-        for(int i=0;i<V;i++)
-        {
-            if(checkForCycle(i,adj,vis))
-                return false;
-        }
-        return true;
+        return res.size()==num;
     }
 };
