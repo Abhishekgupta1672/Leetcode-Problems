403e5eda3cb3b2c6c493d0793e88f4ca0737bc76
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-23 12:41:16 +0530

    Time: 27 ms (87.38%), Space: 13.7 MB (42.43%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
-    bool validColor(vector<vector<int>>&graph, vector<int>&color, int c,int node)
-    {
-        if(color[node]!=0) return color[node]==c;
-        color[node] = c;
-        for(auto itr : graph[node])
-            if(!validColor(graph,color,-c,itr)) return false;
-        return true;
-    }
     bool isBipartite(vector<vector<int>>& graph) {
         vector<int>color(graph.size());
         for(int i=0;i<graph.size();i++)
-            if(color[i]==0 && !validColor(graph,color,1,i)) return false;
+        {
+            if(color[i]==0)
+            {
+                queue<int>q;
+                q.push(i);
+                color[i] = 1;
+                while(!q.empty())
+                {
+                    int node = q.front();
+                    q.pop();
+                    for(auto itr : graph[node])
+                    {
+                        if(color[itr]==0)
+                        {
+                            q.push(itr);
+                            color[itr] = -color[node];
+                        }
+                        else if(color[itr]==color[node]) return false;
+                    }
+                }
+            }
+        }
         return true;
     }
 };
