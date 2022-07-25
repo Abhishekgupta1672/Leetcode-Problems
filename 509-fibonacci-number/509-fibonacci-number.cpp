7d8a798b8e32c656ea7c9a067975df08e725c503
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-07-25 17:14:29 +0530

    Time: 3 ms (58.55%), Space: 5.9 MB (78.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,11 +3,13 @@ public:
     int fib(int n) {
         if(n<=1)
             return n;
-        vector<int>dp(n+1);
-        dp[0] = 0;
-        dp[1] = 1;
+        int x = 0;
+        int y = 1;
         for(int i=2;i<=n;i++)
-            dp[i] = dp[i-1]+dp[i-2];
-        return dp[n];
+        {
+            y = x+y;
+            x = y-x;
+        }
+        return y;
     }
 };
