6141c753574c7a633f1dd314091b838651a9f412
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-08-01 17:39:05 +0530

    Time: 0 ms (100.00%), Space: 16.9 MB (6.59%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -1,13 +1,31 @@
-
 class Solution {
 public:
-    bool isMirror(TreeNode* left , TreeNode* right)
-    {
-        if(left==NULL || right==NULL) return left==right;
-        if(left->val != right->val) return false;
-        return isMirror(left->left , right->right) && isMirror(left->right , right->left);
-    }
     bool isSymmetric(TreeNode* root) {
-        return root==NULL || isMirror(root->left , root->right);
+        queue<TreeNode*>q1,q2;
+        q1.push(root);
+        q2.push(root);
+        while(!q1.empty() && !q2.empty())
+        {
+            TreeNode* left = q1.front();
+            q1.pop();
+            TreeNode* right = q2.front();
+            q2.pop();
+            if(left->val != right->val) return false;
+            if(left->left && right->right)
+            {
+                q1.push(left->left);
+                q2.push(right->right);
+            }
+            else if(left->left || right->right)
+                return false;
+            if(left->right && right->left)
+            {
+                q1.push(left->right);
+                q2.push(right->left);
+            }
+            else if(left->right || right->left)
+                return false;
+        }
+        return true;
     }
 };
