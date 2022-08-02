183b0fb6489c8d87ed89e204c118ce05f7780452
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-08-02 13:33:15 +0530

    Time: 8 ms (58.22%), Space: 12.5 MB (97.50%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    TreeNode* bstFromPreorder(vector<int>& A) {
-        TreeNode * root = new TreeNode(A[0]);
-        if(A.size()==1)
-            return root;
-        for(int i=1;i<A.size();i++)
+    TreeNode* bstFromPreorder(vector<int>& pre) {
+        TreeNode* root = new TreeNode(pre[0]);
+        if(pre.size()==1) return root;
+        for(int i=1;i<pre.size();i++)
         {
             bool done = false;
             TreeNode* cur = root;
             while(done==false)
             {
-                if(cur->val > A[i])
-                {
-                    if(cur->left==NULL)
-                    {
-                        cur->left = new TreeNode(A[i]);
-                        done = true;
-                    }
-                    else
-                        cur = cur->left;
-                }
-                else
+                if(cur->val < pre[i])
                 {
                     if(cur->right==NULL)
                     {
-                        cur->right = new TreeNode(A[i]);
+                        cur->right = new TreeNode(pre[i]);
                         done = true;
                     }
-                    else
+                    else 
                         cur = cur->right;
+                }
+                else
+                {
+                    if(cur->left==NULL)
+                    {
+                        cur->left = new TreeNode(pre[i]);
+                        done = true;
+                    }
+                    else 
+                        cur = cur->left;
                 }
             }
         }
