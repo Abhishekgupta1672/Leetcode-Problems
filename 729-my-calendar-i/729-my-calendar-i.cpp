31c96d84f4fa6441b19f46b49c0624c34117045c
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-08-03 10:26:13 +0530

    Time: 141 ms (67.40%), Space: 37.5 MB (94.67%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -5,8 +5,12 @@ public:
     }
     
     bool book(int start, int end) {
-        for (auto [s, e] : calendar)
-            if (max(s, start) < min(e, end)) return false;
+        for (auto it : calendar)
+        {
+            int s = max(it.first, start);
+            int e = min(it.second, end);
+            if (s < e) return false;
+        }
         calendar.push_back({start, end});
         return true;
     }
