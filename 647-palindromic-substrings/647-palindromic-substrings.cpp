7faf56991c55e3d163b81190b91f2c12ba38a3dc
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-08-08 13:04:26 +0530

    Time: 1348 ms (5.82%), Space: 404.9 MB (7.30%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int count = 0;
-    void findSubstrings(string s, int start, int n) {
-        for(int end = start; end<n; end++)
-            if(isPalindrome(start, end, s))
-                count++;
-    }
+    
     bool isPalindrome(int start, int end, string s) {
         while(start<=end) {
             if(s[start++]!=s[end--]) return false;
         }
         return true;
     }
+    
+    void findSubstrings(string s, int start, int n) {
+        for(int end = start; end<n; end++)
+            if(isPalindrome(start, end, s))
+                count++;
+    }
+
     int countSubstrings(string s) {
         int n = s.size();
         for(int i=0;i<n;i++)
