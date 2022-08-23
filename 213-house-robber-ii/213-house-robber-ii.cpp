025414259199c0ab4c7873d91ed6abbe8c1d347a
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-08-23 17:15:29 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (20.37%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
+    int maxele(vector<int>nums)
+    {
+        int n = nums.size();
+        vector<int>dp(n);
+        dp[0] = nums[0];
+        if(nums.size()==1) return nums[0];
+        dp[1] = max(nums[1],dp[0]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i] = max(dp[i-1] , nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
+    
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums[0];
-        vector<int>dp1(n+1),dp2(n+1);
-        dp1[0] = 0;
-        dp1[1] = nums[0];
-        dp2[0] = 0;
-        dp2[1] = 0;
-        for(int i=2;i<=n;i++)
+        vector<int>tmp1,tmp2;
+        if(nums.size()==1) return nums[0];
+        for(int i=0;i<n;i++)
         {
-            if(i==n) dp1[i] = dp1[i-1];
-            else dp1[i] = max(dp1[i-1],nums[i-1]+dp1[i-2]);
-            dp2[i] = max(dp2[i-1],nums[i-1]+dp2[i-2]);
+            if(i != 0) tmp1.push_back(nums[i]);
+            if(i != n-1) tmp2.push_back(nums[i]);
         }
-        return max(dp1[n],dp2[n]);
+        return max(maxele(tmp1),maxele(tmp2));
     }
 };
