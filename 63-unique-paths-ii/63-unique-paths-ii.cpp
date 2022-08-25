14586faa212f254cb8a195c99259aba34f2858a2
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-08-25 18:11:54 +0530

    Time: 4 ms (66.92%), Space: 8 MB (13.21%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int rec(int i,int j,vector<vector<int>>&nums,vector<vector<int>>&dp){
-        if(i>=0 && j>=0 && nums[i][j]==1) return 0;
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int left = rec(i,j-1,nums,dp);
-        int up = rec(i-1,j,nums,dp);
-        return dp[i][j] = left+up;
-    }
-    
-    int uniquePathsWithObstacles(vector<vector<int>>& nums) {
+    int uniquePathsWithObstacles(vector<vector<int>>&nums) {
         int m = nums.size();
         int n = nums[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return rec(m-1,n-1,nums,dp);
+        // if(nums.size()==1) return nums[0][0];
+        if(nums[0][0]==1) return 0;
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0] = 1;
+        for(int i=1;i<m;i++)
+            dp[i][0] = (nums[i][0]==0 &&  dp[i-1][0]==1)?1:0;
+        for(int i=1;i<n;i++)
+            dp[0][i] = (nums[0][i]==0 &&  dp[0][i-1]==1)?1:0;
+        
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(nums[i][j]==0)
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                else
+                    dp[i][j] = 0;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
