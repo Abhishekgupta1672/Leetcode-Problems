af304de84c306adc33c4440643ac5f3179e04c3d
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-09-05 12:06:13 +0530

    Time: 27 ms (76.99%), Space: 11.1 MB (90.82%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    void solve(Node* root , vector<int>&res){
-        if(!root) return;
-        res.push_back(root->val);
-        for(Node* child : root->children){
-            solve(child,res);
-        }
-    }
-    
     vector<int> preorder(Node* root) {
         vector<int>res;
-        solve(root,res);
+        if(!root) return res;
+        stack<Node*>st;
+        st.push(root);
+        while(!st.empty()){
+            Node* tmp = st.top();
+            st.pop();
+            res.push_back(tmp->val);
+            for(int i=tmp->children.size()-1;i>=0;i--){
+                if(tmp->children[i] != NULL) st.push(tmp->children[i]);
+            }
+        }
         return res;
     }
 };
