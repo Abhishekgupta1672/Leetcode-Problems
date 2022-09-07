9cd4dd5d785b953e94ed54d50f31f5c9390e819b
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-09-07 23:27:18 +0530

    Time: 82 ms (5.27%), Space: 32.8 MB (41.62%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -1,20 +1,18 @@
+
 class Solution {
 public:
-    TreeNode* del(TreeNode* &root)
-    {
+    TreeNode* del(TreeNode* &root){
         if(!root->left) return root;
         return del(root->left);
     }
+    
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(!root) return NULL;
-        if(root->val > key)
-            root->left = deleteNode(root->left,key);
-        else if(root->val < key)
-            root->right = deleteNode(root->right,key);
-        if(root->val == key)
-        {
-            if(!root->right) return root->left;
+        if(root->val > key) root->left = deleteNode(root->left,key);
+        else if(root->val < key) root->right = deleteNode(root->right,key);
+        if(root->val == key){
             if(!root->left) return root->right;
+            if(!root->right) return root->left;
             else{
                 TreeNode* newNode = del(root->right);
                 int tmp = newNode->val;
