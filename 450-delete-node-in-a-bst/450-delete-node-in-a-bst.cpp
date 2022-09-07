04234d67ec6efd883683c9edc6ea1255cedcb534
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-09-07 23:36:35 +0530

    Time: 67 ms (25.61%), Space: 32.5 MB (97.23%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -1,23 +1,36 @@
 
 class Solution {
 public:
-    TreeNode* del(TreeNode* &root){
-        if(!root->left) return root;
-        return del(root->left);
-    }
+    // TreeNode* del(TreeNode* &root){
+    //     if(!root->left) return root;
+    //     return del(root->left);
+    // }
     
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(!root) return NULL;
-        if(root->val > key) root->left = deleteNode(root->left,key);
-        else if(root->val < key) root->right = deleteNode(root->right,key);
+        if(root->val > key){
+            root->left = deleteNode(root->left,key);
+        }
+        else if(root->val < key){
+            root->right = deleteNode(root->right,key);
+        }
         if(root->val == key){
             if(!root->left) return root->right;
             if(!root->right) return root->left;
             else{
-                TreeNode* newNode = del(root->right);
-                int tmp = newNode->val;
-                root->val = tmp;
-                root->right = deleteNode(root->right,tmp);
+                if(!root->left && !root->right){
+                    return NULL;
+                }
+                if(!root->left || !root->right){
+                    TreeNode* ptr = root->right?root->right:root->left;
+                    return ptr;
+                }
+                TreeNode* itr = root->right;
+                while(itr->left){
+                    itr = itr->left;
+                }
+                swap(itr->val,root->val);
+                root->right = deleteNode(root->right,itr->val);
             }
         }
         return root;
