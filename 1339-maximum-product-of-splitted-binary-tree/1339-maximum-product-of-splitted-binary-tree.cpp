109d5824737a233b2f5773a4633ce7807d03b87f
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-09-08 12:40:50 +0530

    Time: 294 ms (17.43%), Space: 77.3 MB (91.50%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long int ans = 0 , total = 0;
+    long int solve(TreeNode* root){
+        if(!root) return 0;
+        long int cur = solve(root->left)+solve(root->right)+root->val;
+        ans = max(ans,cur*(total-cur));
+        return cur;
+    }
+    
+    long int maxProduct(TreeNode* root) {
+        total = solve(root);
+        solve(root);
+        return ans%int(1e9+7);
+    }
+};
