b7b3a5afc1a154deacb5ad368929eb3c56e61e10
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-09-24 11:57:17 +0530

    Time: 20 ms (54.55%), Space: 38.8 MB (12.79%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -1,31 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void rec(TreeNode* root , vector<int>temp , int target,vector<vector<int>>&res){
-        if(root==NULL) return;
-        if(!root->left && !root->right && target==root->val){
+    void rec(TreeNode* root, vector<int>temp,int ts,vector<vector<int>>&res){
+        if(!root) return ;
+        if(!root->right && !root->left && ts == root->val){
             temp.push_back(root->val);
             res.push_back(temp);
             return;
         }
         temp.push_back(root->val);
-        rec(root->left,temp,target-root->val,res);
-        rec(root->right,temp,target-root->val,res);
+        rec(root->left,temp,ts-root->val,res);
+        rec(root->right,temp,ts-root->val,res);
     }
     
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>>res;
-        rec(root, {}, targetSum, res);
+        rec(root,{},targetSum,res);
         return res;
     }
 };
