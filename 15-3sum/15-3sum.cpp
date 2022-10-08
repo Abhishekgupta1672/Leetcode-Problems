31ccdd661115ad31523bbc99909239496bb7ecf1
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-10-08 09:21:40 +0530

    Time: 164 ms (52.29%), Space: 20 MB (63.39%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,28 +2,21 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>>v;
-        sort(nums.begin() , nums.end());
         int n = nums.size();
-        if(n<3) return v;
-        if(nums[0]>0) return v;
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]>0) break;
+        sort(nums.begin(),nums.end());
+        if(nums[0]>0 || n<3) return {};
+        for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
+            if(nums[i]>0) break;
             int left = i+1 , right = n-1;
-            while(left<right)
-            {
-                if(nums[i]+nums[left]+nums[right] > 0) right--;
-                else if(nums[i]+nums[left]+nums[right] < 0) left++;
-                else
-                {
-                    v.push_back({nums[i] , nums[left] , nums[right]});
+            while(left<right){
+                if(nums[left]+nums[right]+nums[i] < 0) left++;
+                else if(nums[left]+nums[right]+nums[i] > 0) right--;
+                else{
+                    v.push_back({nums[left],nums[right],nums[i]});
                     int last_left = nums[left] , last_right = nums[right];
-                    while(left<right && last_left==nums[left])
-                        left++;
-                    while(left<right && last_right==nums[right])
-                        right--;
+                    while(left<right && nums[left]==last_left) left++;
+                    while(left<right && nums[right]==last_right) right--;
                 }
             }
         }
