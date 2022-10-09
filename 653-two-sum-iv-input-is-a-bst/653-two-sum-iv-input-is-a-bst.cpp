9ff83c560dbfb8224f8db3cf3e193a9f91cd2ff6
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-10-09 08:21:58 +0530

    Time: 98 ms (21.91%), Space: 36.9 MB (62.65%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,32 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool findTarget(TreeNode* root, int target) {
-        stack<TreeNode*>st;
+    void rec(TreeNode* root , vector<int>&res){
+        if(!root) return;
+        rec(root->left,res);
+        res.push_back(root->val);
+        rec(root->right,res);
+    }
+    
+    bool findTarget(TreeNode* root, int k) {
         vector<int>res;
-        while(!st.empty() || root!=NULL)
-        {
-            if(root)
-            {
-                st.push(root);
-                root = root->left;
-            }
-            else
-            {
-                root = st.top();
-                st.pop();
-                res.push_back(root->val);
-                root = root->right;
-            }
-        }
-        int i = 0 , j = res.size()-1;
-        while(i<j)
-        {
-            if(res[i]+res[j]==target)
+        rec(root,res);
+        int i=0 , j = res.size()-1;
+        while(i<j){
+            if(res[i]+res[j]==k){
                 return true;
-            else if(res[i]+res[j]>target)
-                j--;
-            else
-                i++;
+            }
+            else if(res[i]+res[j]>k) j--;
+            else i++;
         }
         return false;
     }
