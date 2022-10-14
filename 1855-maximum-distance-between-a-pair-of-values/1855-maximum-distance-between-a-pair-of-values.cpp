ac3037a923d2da845073f818cd5786e1cc295f70
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-10-14 22:00:32 +0530

    Time: 479 ms (23.45%), Space: 98.6 MB (27.07%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
+    int bsearch(vector<int>&nums2 , int target , int start){
+        int l = start , h = nums2.size()-1;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(nums2[mid] < target) h = mid-1;
+            else l = mid+1;
+        }
+        if(h<0) return -1;
+        return nums2[h]>=target?h:-1;
+    }
+    
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0 , j = 0 , m = nums1.size() , n = nums2.size() , res = 0;
-        while(i<m && j<n){
-            if(nums1[i] > nums2[j]) i++;
-            else res = max(res,j++ - i);
+        int res = 0;
+        int n = nums1.size();
+        for(int i=0;i<n;i++){
+            int j = bsearch(nums2,nums1[i],i);
+            if(j==-1) continue;
+            res = max(res,j-i);
         }
         return res;
     }
