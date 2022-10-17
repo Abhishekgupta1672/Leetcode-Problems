02324adb292a06ff757b3b051706b5945320d57f
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-10-17 16:46:54 +0530

    Time: 1130 ms (57.32%), Space: 93.8 MB (16.56%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_map<string,int>mp;
+        for(auto x:ideas){
+            mp[x]++;
+        }
+        vector<vector<long long>>res(26,vector<long long>(26,0));
+        for(int i=0;i<ideas.size();i++){
+            string words = ideas[i].substr(1);
+            int in = ideas[i][0]-'a';
+            for(int j=0;j<26;j++){
+                char c = 'a'+j;
+                string temp = c+words;
+                if(mp.count(temp)==0){
+                    res[in][j] += 1;
+                }
+            }
+        }
+        long long cnt = 0;
+        for(int i=0;i<26;i++){
+            for(int j=0;j<26;j++){
+                if(i==j) continue;
+                if(res[i][j]>0){
+                    cnt+=(res[i][j]*res[j][i]);
+                }
+            }
+        }
+        return cnt;
+    }
+};
