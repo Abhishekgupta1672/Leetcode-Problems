78e5563a836e3786b9e45ec236ac697c02c67d34
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-10-24 12:19:23 +0530

    Time: 16 ms (31.91%), Space: 7.8 MB (27.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        string str = "";
-        int n = s.size();
-        for(int i=0;i<n;i++)
-        {
-            if(isalnum(s[i]))
-                str+=tolower(s[i]);
+    bool validpalindrome(string s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return 0;
+            }else{
+                i++;j--;
+            }
+            
         }
-        string p = str;
-        int i=0,j = str.size()-1;
-        while(i<j)
-            swap(str[i++],str[j--]);
-        if(p==str)
-            return true;
-        else
-            return false;
+        return 1;
+    }
+    bool isPalindrome(string s) {
+        string temp="";
+        for(auto x:s){
+            if(isalnum(x)) temp.push_back(tolower(x));
+        }
+        return (validpalindrome(temp));
     }
 };
