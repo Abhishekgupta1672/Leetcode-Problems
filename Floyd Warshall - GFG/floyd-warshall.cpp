797a420273c5d218360fac0c3b2b18b2ef41a7eb
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2022-11-07 11:37:37 +0530

    Added solution - LeetHub

---
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+  const int MAX = 1e7;
+	void shortest_distance(vector<vector<int>>&dp){
+	    // Code here
+	    int n = dp.size();
+	    for(auto &v : dp){
+	        for(int &i : v){
+	            i = (i == -1)?MAX:i;
+	        }
+	    }
+	            for(int k = 0 ; k < n ; k++){
+	    for(int i = 0 ; i < n ; i++){
+	        for(int j = 0 ; j < n ; j++){
+	                dp[i][j] = min(dp[i][j],dp[i][k]+dp[k][j]);
+	            }
+	        }
+	    }
+	    for(auto &v : dp){
+	        for(int &i : v){
+	            i = (i >= MAX)?-1:i;
+	        }
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>matrix(n, vector<int>(n, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution obj;
+		obj.shortest_distance(matrix);
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cout << matrix[i][j] << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
