d3ad3c59505e524aa23223ad7f2dc9cacc6fcb8b
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2023-01-23 10:31:13 +0530

    Added solution - LeetHub

---
+++ Geeks And The String - GFG/geeks-and-the-string.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string removePair(string s) {
+        string ans = "";
+        for(auto x : s){
+            if(ans.empty() || ans.back() != x){
+                ans += x;
+            }
+            else ans.pop_back();
+        }
+        return ans.size()?ans:"-1";
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s; 
+        cin>>s;
+        
+        Solution obj;
+        string res = obj.removePair(s);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
