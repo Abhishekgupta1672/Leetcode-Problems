737079d08acfb3472cc727e64361f371bf75cc7a
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2023-02-08 08:43:19 +0530

    Time: 13 ms (87.61%), Space: 16.6 MB (84.26%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int curreach=0;
+        int maxreach=0;
+        int jump = 0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            maxreach = max(maxreach,nums[i]+i);
+            if(i==curreach)
+            {
+                jump++;
+                curreach=maxreach;
+            }
+        }
+        return jump;
+    }
+};
