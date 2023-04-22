e0c83e192c28f4ccfd8ef450dd893dd5fba7474d
Abhishek Gupta <75482978+Abhishekgupta1672@users.noreply.github.com>
2023-04-22 09:45:55 +0530

    Time: 30 ms (27.14%), Space: 6.8 MB (86.67%) - LeetHub

---
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minMovesToMakePalindrome(string s) {
+        int left = 0,  right = s.size()-1, ans = 0;
+        while(left < right){
+            int l = left, r = right;
+            while(s[l] != s[r]) r--;
+            if(l==r){
+                swap(s[r],s[r+1]);
+                ans++;
+                continue;
+            }
+            else{
+                while(r<right){
+                    swap(s[r],s[r+1]);
+                    r++;
+                    ans++;
+                }
+            }
+            left++, right--;
+        }
+        return ans;
+    }
+};
